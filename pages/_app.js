3e168b4e68038a4acee8e86eae3f222a34da63e0
Steven Ramos <stevenjramossalazar@gmail.com>
2025-10-16 14:30:01 -0400

    feat: integrate web3-react for wallet connection

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { getLibrary } from "../config/web3";
+import { Web3BaseProvider } from "@web3-react/core";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <Web3BaseProvider getLibrary={getLibrary}>
+    <Component> ( ...pageProps)</Component>
+  </Web3BaseProvider>
+  )
 }
